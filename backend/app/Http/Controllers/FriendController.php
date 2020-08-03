5df2fea206f221bfdf22c26fe72a4c8259a3bcb0
tokito1 <mmsbprog@gmail.com>
2020-08-04 01:01:21 +0900

    add friend main function

--- backend/app/Http/Controllers/FriendController.php
+++ backend/app/Http/Controllers/FriendController.php
@@ -10,15 +10,10 @@ class FriendController extends Controller
 {
     public function index()
   {
-      // DBよりBookテーブルの値を全て取得
-      //$users = User::all();
 
       $friends = \Auth::User()->friends()->get(); //ログインユーザ用
       //$friends = User::find(16547823)->friends()->get(); //test
       return view('friend/index', compact('friends'));
-
-      // 取得した値をビュー「book/index」に渡す
-      //return view('friend/index', compact('users'));
   }
 
   public function store(FriendRequest $request)
@@ -33,20 +28,10 @@ class FriendController extends Controller
 
   public function delete(Request $request)
   {
-      $user_id = \Auth::User()->user_id;
-      DB::table('friends')
-      ->where('user_id', $user_id)
-      ->where('friend_id', $request->friend_id)
-      ->delete();
-      //$user = \Auth::User();
-      //$user->friends()->where('friend_id', $request->friend_id)->delete();
+      $user_id = \Auth::User()->id;
 
-      //$frineds = \Auth::User()->friends()->get();
-      /*
-      foreach($frineds as $friend){
-          if ($friend->id == $request->friend_id)
-            ;
-      }*/
+      $user = \Auth::User();
+      $user->friends()->detach($request->friend_id);
       return redirect('friend')->with('success', '友達を削除しました');
       
   }
